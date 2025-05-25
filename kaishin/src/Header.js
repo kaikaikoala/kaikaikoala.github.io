cef46246f92094b4bbc9b26d938c0bfe2c687728
badtechlead <badtechlead69>
2025-05-24 22:56:13 -0700

    Add header

---
+++ kaishin/src/Header.js
@@ -0,0 +1,65 @@
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import logo from './logo.svg';
+import nyc_skyline_afternoon from './assets/nyc_skyline_afternoon.jpg';
+import './App.css';
+
+function Header() {
+  return (
+    <Box sx={{
+        backgroundImage: `url(${nyc_skyline_afternoon})`,
+        backgroundSize: 'cover',         // scales image to fill container
+        backgroundPosition: 'center',    // centers the image
+        backgroundRepeat: 'no-repeat',   // avoids tiling
+        height: '100vh',                 // full viewport height
+        width: '100%',                   // full width
+        mb: 4, // margin-bottom (MUI spacing scale, 4 = 32px)
+      }}>
+      <Container sx={{
+        height: '100vh',           // full viewport height
+        display: 'flex',
+        justifyContent: 'center',  // horizontal center
+        alignItems: 'center',      // vertical center
+      }}>
+        <header>
+          <Typography variant="h1" component="h1" align="center">
+            Kai Kawada
+          </Typography>
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            direction="row"
+            spacing={1}>
+            <Chip
+              sx={(theme) => ({
+                  backgroundColor: theme.palette.background.paper,
+                  color: theme.palette.text.primary, // ensure readable text color
+                })}
+              label="Software engineer"
+              component="a"
+              clickable
+              href="#SectionSwe"
+               />
+            <Chip
+              sx={(theme) => ({
+                  backgroundColor: theme.palette.background.paper,
+                  color: theme.palette.text.primary, // ensure readable text color
+                })}
+              label="Tennis enthusiast"
+              component="a"
+              href="#SectionTennis"
+              clickable
+              />
+          </Stack>
+        </header>
+      </Container>
+    </Box>
+  );
+}
+
+export default Header;
---
+++ kaishin/src/SectionSwe.js
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+
+function MainSwe() {
+  return (
+    <Container>
+      <section id="SectionSwe">
+        <Typography variant="h2" component="h2">
+          Software engineer
+        </Typography>
+      </section>
+    </Container>
+      );
+}
+
+export default MainSwe;
